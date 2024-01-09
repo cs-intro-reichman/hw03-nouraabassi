2ce784ce7582fff5506ac81afb35875d88d1b16c
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-09 17:49:36 +0200

    Update LowerCase.java

--- LowerCase.java
+++ LowerCase.java
@@ -1,7 +1,7 @@
 /** String processing exercise 1. */
 /*public class LowerCase {
     public static void main(String[] args) {  
-        String str = args[0];
+        //String str = args[0];
         System.out.println(lowerCase(str));
     }
 
